ea2545924e910ba4e8fa9b2b5fb8f726dd81cb04
Jonathan <jonathan.kupcho@gmail.com>
2024-10-14 15:11:02 -0500

    chore: added htmx types

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,7 +1,14 @@
+import 'typed-htmx'
 import {} from 'hono'
 
 declare module 'hono' {
     interface ContextRenderer {
         (content: string | Promise<string>, props?: { title?: string }): Response
+    }
+}
+
+declare module 'hono/jsx' {
+    namespace JSX {
+        interface HTMLAttributes extends HtmxAttributes {}
     }
 }
